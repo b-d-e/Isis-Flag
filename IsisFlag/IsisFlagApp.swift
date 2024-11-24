a1f2aab389993f8a3ab6f19bebac0c9b90d02cce
Benjamin <betheridge@turing.ac.uk>
2024-11-24 17:42:10 +0000

    Initial Commit

---
+++ IsisFlag/IsisFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  IsisFlagApp.swift
+//  IsisFlag
+//
+//  Created by B E on 24/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct IsisFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
